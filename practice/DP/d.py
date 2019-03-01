055983621ce7e3b8f0969fa016131a08add7e201
shimizu <shimizu@shimizunoMacBook-puro.local>
2019-03-01 12:46:15 +0900

    create d

---
+++ practice/DP/d.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+N = int(input())
+A = list(map(int, input().split()))
+S = int(input())
+print (A)
+dp = [[0 for a in range(len(A)+1)] for s in range(S+1)]
+dp[0][0] = 1
+for i in range(len(A)):
+    print (len(A),i)
+    for s in range(S+1):
+        if s+1-A[i] >= 0:
+            dp[s][i+1] =  dp[s-A[i]][i] + dp[s][i]
+        else:
+            dp[s][i+1] = dp[s][i]
+        dp[s][i+1] %= 1000000009
+print (dp)
+print (dp[-1][-1])
