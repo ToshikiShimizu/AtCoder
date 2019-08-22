700b9d1a9bd179f3a40f6858b93369cfe120b0ed
清水俊樹 <tamreff3290@gmail.com>
2019-08-23 08:38:48 +0900

    108b.cpp

--- ABC/108/b.cpp
+++ ABC/108/b.cpp
@@ -4,18 +4,7 @@ int main(){
     int x1, y1, x2, y2;
     int d;
     cin >> x1 >> y1 >> x2 >> y2;
-    for (int i=0;i<2;i++){
-        if (i!=0)cout << " ";
-        if (x1==x2){
-            d = y1 - y2;
-            y1 = y2;
-            x2 += d;
-        }else{
-            d = x1 - x2;
-            y2 += d;
-            x1 = x2;
-        }
-        cout << x2 << " " << y2;
-    }
-    cout << endl;
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    cout << x2-dy << " " << y2+dx << " " << x1-dy << " " <<y1+dx << endl;
 }
