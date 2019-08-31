7ee351cc0a1efdeccc16f8dbd06b36701b0e619f
清水俊樹 <tamreff3290@gmail.com>
2019-08-31 18:08:26 +0900

    fix 096

--- ABC/096/b.cpp
+++ ABC/096/b.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
 #define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
 #define SORT(a) sort(ALL(a))
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 typedef long long ll;
@@ -19,6 +20,6 @@ int main() {
     }
     cin >> k;
     REP(i,k)*max_element(ALL(vec))*=2;
-    cout << accumulate(ALL(vec),0)<<endl;
+    cout << SUM(vec) <<endl;
 
 }
