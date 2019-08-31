64647780f7b8a29fdf2a2164c11e4a614874d18f
清水俊樹 <tamreff3290@gmail.com>
2019-08-31 18:27:51 +0900

    086a

---
+++ ABC/086/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a*b%2){
+        cout << "Odd" << endl;
+    }else{
+        cout << "Even" << endl;
+    }
+}
