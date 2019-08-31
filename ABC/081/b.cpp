446771eb2275272defa5c20a4ccd9927f786532d
清水俊樹 <tamreff3290@gmail.com>
2019-08-31 18:42:19 +0900

    081ab

---
+++ ABC/081/b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int n, a;
+  int mn = INF;
+  cin >> n;
+  REP(i,n){
+      cin >> a;
+      int cnt = 0;
+      while(a%2==0){
+          cnt++;
+          a /= 2;
+      }
+      mn = min(mn,cnt);
+  }
+  cout << mn << endl;
+}
