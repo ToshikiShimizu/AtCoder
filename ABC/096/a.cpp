2d47052f3ea42320402bea0ceeb6a2612581c107
清水俊樹 <tamreff3290@gmail.com>
2019-08-31 18:04:44 +0900

    096ab

---
+++ ABC/096/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SORT(a) sort(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+    int a,b;
+    cin >> a >>b;
+    int ans = a;
+    dump(ans);
+    if (a > b)ans--;
+    cout << ans << endl;
+}
---
+++ ABC/096/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SORT(a) sort(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+    vector<int> vec;
+    int n, k;
+    REP(i,3){
+        cin >> n;
+        vec.push_back(n);
+    }
+    cin >> k;
+    REP(i,k)*max_element(ALL(vec))*=2;
+    cout << accumulate(ALL(vec),0)<<endl;
+
+}
