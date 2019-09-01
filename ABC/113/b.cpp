8c60c3d710ff9b046d9b94de4c07498b5687b467
清水俊樹 <tamreff3290@gmail.com>
2019-09-01 18:13:04 +0900

    081b class problem

---
+++ ABC/113/b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int n;
+  double t, a;
+  int h;
+  double mn = INF;
+  int argmin;
+  cin >> n;
+  cin >> t >> a;
+  REP(i,n){
+      cin >> h;
+      if (abs(t - h * 0.006 - a)< mn){
+          mn = abs(t - h * 0.006 - a);
+          argmin = i;
+      }
+
+  }
+  cout << argmin+1 << endl;
+}
