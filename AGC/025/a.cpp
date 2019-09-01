f8b0ada4ead73b2f066445b55e7c5f96d9c6e9b7
清水俊樹 <tamreff3290@gmail.com>
2019-09-01 20:26:03 +0900

    083b class problem

---
+++ AGC/025/a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int DigitSum(int n){
+    int sum = 0;
+    while(n>0){
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  int a,b;
+  int ans = INF;
+  FOR(a,1,n/2+1){
+      b = n-a;
+      ans = min(ans, DigitSum(a)+DigitSum(b));
+  }
+  cout << ans << endl;
+}
+  
