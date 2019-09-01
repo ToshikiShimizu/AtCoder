31023a6c7dc06cdd00985a9ba37499bee945b6d9
清水俊樹 <tamreff3290@gmail.com>
2019-09-01 23:23:03 +0900

    ABC139

---
+++ ABC/139/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  string S;
+  string T;
+  cin >> S >> T;
+  int ans = 0;
+  REP(i,S.size()){
+      if(S[i]==T[i])ans++;
+  }
+  cout << ans << endl;
+}
---
+++ ABC/139/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int A, B;
+  cin >> A >>B;
+  REP(i,20){
+      if(1 + i * (A-1) >= B){
+          cout << i << endl;
+          return 0;
+      }
+  }
+}
