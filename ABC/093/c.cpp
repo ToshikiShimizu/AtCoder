23d8e2e2b94daf8aecce74fe2c8e44d4404bee4d
清水俊樹 <tamreff3290@gmail.com>
2019-09-06 08:02:58 +0900

    086c class problem

---
+++ ABC/093/c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INTINF = 1<<30;
+const ll LLINF = 1LL<<62;
+const int MOD = (int)1e9 + 7;
+const double EPS = 1e-9;
+
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int a,b,c;
+  cin >> a >> b >> c;
+  int mx = max(max(a,b),c);
+  int sm = a+b+c;
+  if ((sm+mx)%2==1){
+      mx++;
+  }
+  cout << (3*mx-sm)/2 << endl;
+}
