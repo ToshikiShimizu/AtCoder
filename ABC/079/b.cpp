3b5a37723b185d850cc5d37eb795ca1051a4944d
清水俊樹 <tamreff3290@gmail.com>
2019-09-14 10:58:01 +0900

    079ab

---
+++ ABC/079/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INTINF = 1<<29;
+const ll LLINF = 1LL<<61;
+const int MOD = (int)1e9 + 7;
+const double EPS = 1e-9;
+const int DX[8]={ 0, 1, 0,-1, 1, 1,-1,-1};
+const int DY[8]={ 1, 0,-1, 0, 1,-1, 1,-1};
+
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  ll L[87];
+  L[0] = 2;
+  L[1] = 1;
+  FOR(i,2,87){
+      L[i] = L[i-1]+L[i-2];
+  }
+  int n;
+  cin >> n;
+  cout << L[n] << endl;
+}
