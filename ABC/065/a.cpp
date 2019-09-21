489caa9265bba90bd375dd66f84e6ba85174d49e
清水俊樹 <tamreff3290@gmail.com>
2019-09-21 10:56:15 +0900

    64 65 ab

---
+++ ABC/065/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INTINF = 1<<29;
+const ll LLINF = 1LL<<61;
+const int MOD = (int)1e9 + 7;
+const double EPS = 1e-9;
+const int DX[8]={ 0, 1, 0,-1, 1, 1,-1,-1};
+const int DY[8]={ 1, 0,-1, 0, 1,-1, 1,-1};
+
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int x,a,b;
+  cin >> x >> a >> b;
+  if (a-b>=0){
+      cout << "delicious" << endl;
+  }else if(a+x-b>=0){
+      cout << "safe" << endl;
+  }else{
+      cout << "dangerous"  << endl;
+  }
+}
