20aec06ed93d747d57359bfefe3eb8cfcda97eec
清水俊樹 <tamreff3290@gmail.com>
2020-04-12 15:14:34 +0900

    abc023d

---
+++ ABC/023/d.py
@@ -0,0 +1,28 @@
+def is_ok(x):
+    ls = []
+    for i in range(N):
+        ls.append((x-H[i])//S[i])
+    ls.sort()
+    for n in range(N):
+        if (ls[n] < n):
+            return False
+    return True
+
+def meguru_bisect(ng, ok):
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+N = int(input())
+H = []
+S = []
+for i in range(N):
+    h, s = map(int,input().split())
+    H.append(h)
+    S.append(s)
+print (meguru_bisect(0,int(1e+14)))
+
