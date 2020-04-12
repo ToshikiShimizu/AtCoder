a351794eb44e3b83d34a48773e0b65cee4781dc8
清水俊樹 <tamreff3290@gmail.com>
2020-04-12 16:46:54 +0900

    fix

--- AGC/043/a.py
+++ AGC/043/a.py
@@ -9,9 +9,10 @@ dp[1][0] = 0
 
 for h in range(H):
     for w in range(W):
-        dp[h+1][w+1] = min(dp[h][w+1] + (board[h][w]=='#' and (h==0 or board[h-1][w]=='.')), dp[h+1][w] + (board[h][w]=='#' and (w==0 or board[h][w-1]=='.')))
- 
-            
+        dp[h+1][w+1] = min(
+            dp[h][w+1] + (board[h][w]=='#' and (h==0 or board[h-1][w]=='.')),
+            dp[h+1][w] + (board[h][w]=='#' and (w==0 or board[h][w-1]=='.'))
+        )
 
 print (dp[-1][-1])
 
