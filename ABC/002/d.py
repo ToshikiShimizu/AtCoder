2cf59a23a19eeb6a642baccdebe1a0213502b324
清水俊樹 <tamreff3290@gmail.com>
2020-04-14 19:42:16 +0900

    abc002d

---
+++ ABC/002/d.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+N, M = map(int, input().split())
+link = [[0 for i in range(N)] for j in range(N)]
+for m in range(M):
+    x, y = map(int, input().split())
+    link[x-1][y-1] = 1
+    link[y-1][x-1] = 1
+
+for i in range(N,1,-1):
+    for comb in combinations(range(N),i):
+        ok = True
+        for c in combinations(comb, 2):
+            if (link[c[0]][c[1]] == 0):
+                ok = False
+                break
+        if ok:
+            print (i)
+            exit()
+print (1)
