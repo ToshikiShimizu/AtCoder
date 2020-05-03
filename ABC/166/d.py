683f36cfff7280e24c6df9b8b0647e4637ae9764
清水俊樹 <tamreff3290@gmail.com>
2020-05-04 08:51:02 +0900

    fix d

--- ABC/166/d.py
+++ ABC/166/d.py
@@ -5,13 +5,19 @@ INF = 10**10
 MOD = 10**9 + 7
 X = int(input())
 MAX_X = 10**9
-n = 0
+a_max = 0
 while(1):
-    n+=1
-    if n**5 - (n-1)**5 > MAX_X:
+    a_max+=1
+    if a_max**5 - (a_max-1)**5 > MAX_X:
         break
-for i in range(n):
-    for j in range(-n+1,n-1):
+b_min = 0
+while(1):
+    b_min-=1
+    if -(b_min)**5 > MAX_X:
+        break
+#print (a_max, b_min)
+for i in range(a_max):
+    for j in range(b_min+1,a_max-1):
         if i**5 - j**5 == X:
             print (i,j)
             exit()
