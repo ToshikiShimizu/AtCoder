c1b3d478bb859d9408f222d66164a0d2b5f56774
清水俊樹 <tamreff3290@gmail.com>
2020-05-05 23:25:37 +0900

    arc067a

--- ARC/067/a.py
+++ ARC/067/a.py
@@ -1,11 +1,23 @@
-#coding:utf-8
-A, K = map(int, input().split())
-if K==0:
-    print (int(2e+12)-A)
-else:
-    for i in range(42):
-        if(2e+12 <= A):
-            print (i)
-            break
-        print(A)
-        A += 1 + K * A
+MOD = 10**9+7
+def factorize(N):
+    n = N
+    fact = []
+    for i in range(2,int(N**0.5)+1): # intに変換すること
+        while(n%i==0):
+            n //= i #  整数除算(//)を使うこと
+            fact.append(i)
+    if n!=1:
+        fact.append(n)
+    return fact
+N = int(input())
+cnt = [0] * (N+1)
+for n in range(1,N+1):
+    fact = factorize(n)
+    for f in fact:
+        cnt[f] += 1
+
+ans = 1
+for n in range(N+1):
+    ans *= (cnt[n]+1)
+    ans %= MOD
+print (ans)
