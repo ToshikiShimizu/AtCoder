1bc3a177966b597acd796b350ae022d11396189a
清水俊樹 <tamreff3290@gmail.com>
2020-06-14 22:56:44 +0900

    fix

--- ABC/170/b.py
+++ ABC/170/b.py
@@ -1,8 +1,6 @@
 x, y = map(int,input().split())
-
 for a in range(x+1):
     b = x-a
-    #print(a,b)
     if 2*a+4*b==y:
         print ('Yes')
         exit()
--- ABC/170/c.py
+++ ABC/170/c.py
@@ -1,11 +1,8 @@
 X, N = map(int,input().split())
 P = list(map(int,input().split()))
 P.sort()
-
 diff = 10**10
-ans = -1
 for p in range(102):
-    #print(p)
     if p in P:
         continue
     if abs(X-p) < diff:
