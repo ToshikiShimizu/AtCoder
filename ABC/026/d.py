bfed37e8dfac32bd943d38730664460f05dbc693
清水俊樹 <tamreff3290@gmail.com>
2020-06-19 21:51:03 +0900

    fix

--- ABC/026/d.py
+++ ABC/026/d.py
@@ -3,7 +3,7 @@ a,b,c = map(int,input().split())
 def func(t):
     return a * t + b * math.sin(c*t*math.pi)
 l = 0
-r = 200
+r = 400
 for i in range(100):
     mid = (l + r) / 2
     if func(mid)>100:
