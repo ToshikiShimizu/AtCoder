39a433fcfd15d3686f23b10f8d15581eadd1bfca
清水俊樹 <tamreff3290@gmail.com>
2020-06-19 21:38:57 +0900

    abc026d

---
+++ ABC/026/d.py
@@ -0,0 +1,13 @@
+import math
+a,b,c = map(int,input().split())
+def func(t):
+    return a * t + b * math.sin(c*t*math.pi)
+l = 0
+r = 200
+for i in range(100):
+    mid = (l + r) / 2
+    if func(mid)>100:
+        r = mid
+    else:
+        l = mid
+print (mid)
