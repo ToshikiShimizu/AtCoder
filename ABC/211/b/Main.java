7a7ecc7def2416b2fb100c656d0b416a675f422d
清水俊樹 <tamreff3290@gmail.com>
2021-07-29 09:44:44 +0900

    211b他の提出をみてリファクタ

--- ABC/211/b/Main.java
+++ ABC/211/b/Main.java
@@ -4,15 +4,12 @@ import java.util.HashSet;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String answer = "Yes";
         HashSet<String> set = new HashSet<>();
-        for (int i=0;i<4;i++){
+        int Size = 4;
+        for (int i=0;i<Size;i++){
             String s = sc.next();
-            if (set.contains(s)){
-                answer = "No";
-            }
             set.add(s);
         }
-        System.out.println(answer);
+        System.out.println(set.size()==Size ? "Yes" : "No");
     }
 }
