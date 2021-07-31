d2561b43b3bdfbc2960b77c4e2a2cab0ddb01caf
清水俊樹 <tamreff3290@gmail.com>
2021-08-01 08:44:09 +0900

    187b

---
+++ ABC/187/b/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        double[][] point = new double[n][2];
+        for (int i=0; i<n; i++){
+            point[i][0] = sc.nextInt();
+            point[i][1] = sc.nextInt();
+        }
+        for (int i=0; i<n; i++){
+            for (int j=i+1; j<n; j++){
+                double angle = (point[j][1] - point[i][1])/(point[j][0] - point[i][0]);
+                if (-1 <= angle && angle <= 1){
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
