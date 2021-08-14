bfd898f9139e66591cae1e244a225d0195ac433c
清水俊樹 <tamreff3290@gmail.com>
2021-08-14 10:10:28 +0900

    196b

---
+++ ABC/196/b/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        String x = sc.next();
+        String[] ans = x.split("\\.");
+        System.out.println(ans[0]);
+    }
+}
