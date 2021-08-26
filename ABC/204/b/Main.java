e720dfe3f37d0e1bb9cff1f7eb1294352a83a075
清水俊樹 <tamreff3290@gmail.com>
2021-08-26 11:03:35 +0900

    204b

---
+++ ABC/204/b/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        for (int i=0; i<n; i++){
+            int a = sc.nextInt();
+            if (a > 10){
+                ans += a - 10;
+            }
+        }
+        System.out.println(ans);
+    }
+}
