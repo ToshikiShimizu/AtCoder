8b2450d2b5b63d2f68b1a6c4c0b18ff86133e6c0
清水俊樹 <tamreff3290@gmail.com>
2021-09-10 08:24:24 +0900

    zone2021a

---
+++ zone/2021/a/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.regex.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] n = s.split("ZONe", -1);
+        System.out.println(n.length-1);
+
+    }
+}
