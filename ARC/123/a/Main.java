68383d0670159a4ad722cb219279b0a55ff93c53
清水俊樹 <tamreff3290@gmail.com>
2021-09-30 08:31:17 +0900

    arc123a

---
+++ ARC/123/a/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        long x = 2 * b - a - c;
+        long ans = x + 3 * (Math.max(0, (1-x)/2));
+        System.out.println(ans);
+    }
+}
