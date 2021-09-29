bda4d832aa26fdc86665a4db8e02e3c1ca9937f3
清水俊樹 <tamreff3290@gmail.com>
2021-09-29 19:35:52 +0900

    arc120a

--- ARC/120/a/Main.java
+++ ARC/120/a/Main.java
@@ -5,14 +5,14 @@ public class Main{
         int n = sc.nextInt();
         long [] A = new long [n];
         long max = 0;
-        long sum = 0;
-        long lastmax = 0;
+        long s = 0;
+        long sum_s = 0;
         for (int i=0; i<n;i++){
             A[i] = sc.nextLong();
             max = Math.max(max, A[i]);
-            sum += lastmax + A[i];
-            lastmax += A[i];
-            long ans = sum + max * (i+1);
+            s += A[i];
+            sum_s += s;
+            long ans = sum_s + max * (i+1);
             System.out.println(ans);
         }
     }
